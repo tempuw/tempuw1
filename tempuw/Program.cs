b72c550d611003e62580aaa112eb6e0a01bfb128
Никита <nikitka.kozyrew@mail.ru>
2025-07-24 00:59:51 +0400

    Initial console app

---
+++ tempuw/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace tempuw;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    var tanks = GetTanks();
+    var units = GetUnits();
+    var factories = GetFactories();
+    Console.WriteLine($"Количество резервуаров: {tanks.Length}, установок: {units.Length}");
+
+    var foundUnit = FindUnit(units, tanks, "Резервуар 2");
+    var factory = FindFactory(factories, foundUnit);
+
+    Console.WriteLine($"Резервуар 2 принадлежит установке {foundUnit.Name} и заводу {factory.Name}");
+
+    var totalVolume = GetTotalVolume(tanks);
+    Console.WriteLine($"Общий объем резервуаров: {totalVolume}");
+  }
+
+  // реализуйте этот метод, чтобы он возвращал массив резервуаров, согласно приложенным таблицам
+  // можно использовать создание объектов прямо в C# коде через new, или читать из файла (на своё усмотрение)
+  public static Tank[] GetTanks()
+  {
+    
+  }
+  // реализуйте этот метод, чтобы он возвращал массив установок, согласно приложенным таблицам
+  public static Unit[] GetUnits()
+  {
+    // ваш код здесь
+  }
+  // реализуйте этот метод, чтобы он возвращал массив заводов, согласно приложенным таблицам
+  public static Factory[] GetFactories()
+  {
+    // ваш код здесь
+  }
+
+  // реализуйте этот метод, чтобы он возвращал установку (Unit), которой
+  // принадлежит резервуар (Tank), найденный в массиве резервуаров по имени
+  // учтите, что по заданному имени может быть не найден резервуар
+  public static Unit FindUnit(Unit[] units, Tank[] tanks, string unitName)
+  {
+    // ваш код здесь
+  }
+
+  // реализуйте этот метод, чтобы он возвращал объект завода, соответствующий установке
+  public static Factory FindFactory(Factory[] factories, Unit unit)
+  {
+    // ваш код здесь
+  }
+
+  // реализуйте этот метод, чтобы он возвращал суммарный объем резервуаров в массиве
+  public static int GetTotalVolume(Tank[] units)
+  {
+    // ваш код здесь
+  }
+}
+
+/// <summary>
+/// Установка
+/// </summary>
+public class Unit
+{
+  //..
+}
+
+/// <summary>
+/// Завод
+/// </summary>
+public class Factory
+{
+  //..
+}
+
+/// <summary>
+/// Резервуар
+/// </summary>
+public class Tank
+{
+  //..
+}
